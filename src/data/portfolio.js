52054e6cca11190ea309bbabf6480c678dd3039b
Troy Abbott <troy.abs28@gmail.com>
2024-07-18 18:42:47 +1200

    tweaks

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
     {
         title: 'Frontend High-Fidelity Prototype',
-        imgUrl: 'dist/assets/project345.png',
+        imgUrl: 'assets/project345.png',
         stack: ['HTML','CSS','JavaScript'],
         link: 'https://www.google.co.nz/'
     },
     {
         title: 'Image-Proccessing for coin detection',
-        imgUrl: 'dist/assets/imgpro.jpg',
+        imgUrl: 'assets/imgpro.jpg',
         stack: ['OpenGL','Python','Object detection'],
         link: 'https://www.google.co.nz/'
     },
     {
         title: 'Developer Portfolio',
-        imgUrl: 'dist/assets/hqdefault.jpg',
+        imgUrl: 'assets/hqdefault.jpg',
         stack: ['React','Tailwind','Vite'],
         link: 'https://www.google.co.nz/'
     }
