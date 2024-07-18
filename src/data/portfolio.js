8d36c619b61de4387053c5ceb65404f60fd074c8
Troy Abbott <troy.abs28@gmail.com>
2024-07-18 18:24:04 +1200

    Image issue solved

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
     {
         title: 'Frontend High-Fidelity Prototype',
-        imgUrl: '/assets/project345.png',
+        imgUrl: 'dist/assets/project345.png',
         stack: ['HTML','CSS','JavaScript'],
         link: 'https://www.google.co.nz/'
     },
     {
         title: 'Image-Proccessing for coin detection',
-        imgUrl: '/assets/imgpro.jpg',
+        imgUrl: 'dist/assets/imgpro.jpg',
         stack: ['OpenGL','Python','Object detection'],
         link: 'https://www.google.co.nz/'
     },
     {
         title: 'Developer Portfolio',
-        imgUrl: '/assets/hqdefault.jpg',
+        imgUrl: 'dist/assets/hqdefault.jpg',
         stack: ['React','Tailwind','Vite'],
         link: 'https://www.google.co.nz/'
     }
